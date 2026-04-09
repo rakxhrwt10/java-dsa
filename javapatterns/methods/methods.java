fedaf974b77a64fdfe32741e4cbbb9db75853f58
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-04-09 21:14:55 +0530

    method

---
+++ javapatterns/methods/methods.java
@@ -0,0 +1,25 @@
+
+
+public class methods {
+    void printtable(){
+        int ans;
+
+        for(int i=1;i<=10;i++){
+
+
+            ans=2*i;
+            System.out.println("->"+ ans);
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+       
+        methods obj=new methods();
+
+        obj.printtable();
+
+    }
+    
+}
