03b1fc0fe1e9ac7cd11ed7d39cbbf7c46d52cac8
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-04-09 17:24:40 +0530

    rombus

---
+++ javapatterns/rombus.java
@@ -0,0 +1,32 @@
+
+
+
+
+public class rombus {
+
+    public static void main(String[] args) {
+        
+
+        int n=5;
+
+        for(int row=1;row<=n;row++){
+
+
+            for(int space=n-row;space>=1;space--){
+                System.err.print(" ");
+
+
+            }
+            for(int col=1;col<=n;col++){
+                System.err.print("*");
+
+            }
+
+            System.err.println();
+
+        }
+    }
+
+
+    
+}
