ed4846a09780e002cafbfffaabfe94b5e2c26791
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-04-09 19:06:25 +0530

    2multiplypyramid

---
+++ javapatterns/Ppyramidstep.java
@@ -0,0 +1,35 @@
+
+
+public class Ppyramidstep {
+
+    public static void main(String[] args) {
+        
+
+        int step=1;
+
+        int n=5;
+
+        for(int row=1;row<=5;row++){
+
+
+            for(int space=n-row;space>=1;space--){
+
+                System.out.print(" ");
+            }
+
+            for(int col=1;col<=2*row-1;col++){
+
+               
+
+                System.out.print("+");
+                
+            }
+            
+            
+
+            System.out.println();
+
+        }
+    }
+    
+}
