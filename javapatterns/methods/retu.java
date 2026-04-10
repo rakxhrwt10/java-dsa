d3720fca531cd96a71693385a8206adfa4ea3b68
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-04-10 11:38:41 +0530

    return

---
+++ javapatterns/methods/retu.java
@@ -0,0 +1,16 @@
+public class retu {
+
+    static int Multiply(int p, int q) {
+        int multi = p * q;   // variable declare
+        return multi;
+    }
+
+    public static void main(String[] args) {
+
+        int result = Multiply(60, 2);   // double nahi, int use karo
+
+        System.out.println(result);
+        System.out.println("Type: int");
+
+    }
+}
