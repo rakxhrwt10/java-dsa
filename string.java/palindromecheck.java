ef149c21f3e09c899f8e4d7beea3aa0d9a28c507
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-04-12 21:30:41 +0530

    palindrome

---
+++ string.java/palindromecheck.java
@@ -0,0 +1,19 @@
+
+
+public class palindromecheck{
+    public static void main(String[] args) {
+        String str = "madam";
+        
+        String rev = "";
+        
+        for(int i = str.length()-1; i >= 0; i--){
+            rev += str.charAt(i);
+        }
+        
+        if(str.equals(rev)){
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
