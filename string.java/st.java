400bb7563e44121a00eda412f7e861b977af76a4
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-04-12 15:30:00 +0530

    string

---
+++ string.java/st.java
@@ -0,0 +1,24 @@
+
+
+public class st {
+
+public static void main(String[] args) {
+    
+
+
+    String str="datascience";
+    System.out.println(str);
+
+    //string is immutable
+
+
+    // str.charAt(0)="new";
+
+    for(int i=0;i<str.length();i++)
+
+
+        System.out.println(str.charAt(i));
+}
+
+    
+}
